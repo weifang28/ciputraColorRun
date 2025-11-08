a72add460c5bdf827a17f9538ff3b5d20ac77581
Arya Athaullah Rafi <aathaullah@student.ciputra.ac.id>
2025-11-08 16:06:16 +0700

    update homepage bagian atas (ARYA)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,53 +19,38 @@ export default function Home() {
     {
       src: TEMP_LOGO_PATH,
       alt: "Partner 3",
-    },
-    {
-      src: TEMP_LOGO_PATH,
-      alt: "Partner 4",
-    },
-    {
-      src: TEMP_LOGO_PATH,
-      alt: "Partner 5",
-    },
-    {
-      src: TEMP_LOGO_PATH,
-      alt: "Partner 6",
-    },
-    {
-      src: TEMP_LOGO_PATH,
-      alt: "Partner 7",
-    },
-    {
-      src: TEMP_LOGO_PATH,
-      alt: "Partner 8",
     }
   ];
 
   return (
     <main className="bg-white">
+
+
       <div className="home_top">
         <div className="home_top_content">
+
           <img
             src="/Images/logo.png"
             alt="Ciputra Color Run Logo"
             className="home_top_logo"
           />
+
           <h1 className="home_title">CIPUTRA COLOR RUN</h1>
 
           <Link href="/register" className="home_register_button">
             REGISTER NOW
           </Link>
-          
+
           <p className="home_description">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate quis aperiam voluptatem perferendis eos distinctio voluptatum exercitationem enim, error dolores iure magnam explicabo vel consectetur ullam excepturi recusandae alias tenetur!
           </p>
+
         </div>
       </div>
 
       <div className="sponsor-container">
 
-        <h1 className="sponsor-title">MAIN SPONSOR</h1>
+        <h1 className="sponsor-title">MA  IN SPONSOR</h1>
 
         <div className="partner-loop-with-borders">
           <LogoLoop
@@ -80,6 +65,7 @@ export default function Home() {
             className="partner-logo-loop"
           />
         </div>
+
       </div>
 
 
