e55304c79478a36d8218e3ff9414d1b7734f5c45
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2025-12-01 15:57:05 +0700

    Alamak

--- src/app/api/admin/payments/confirmed/route.ts
+++ src/app/api/admin/payments/confirmed/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
           include: { jersey: true, category: true },
           orderBy: { id: "asc" },
         },
-        payments: true,
+        payment: true,
       },
       orderBy: { createdAt: "desc" },
     });
